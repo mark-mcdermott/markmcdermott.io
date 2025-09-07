12cfdcdd3b4400cd90e346d1949509f8da295bb4
Mark McDermott <mark@markmcdermott.io>
2025-09-06 19:25:35 -0500

    Build to /docs for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,4 +26,5 @@ export default defineConfig({
     rehypePlugins: [rehypeHeadingIds, rehypeAccessibleEmojis, rehypeKatex],
     remarkPlugins: [remarkToc, remarkMath],
   },
+  outDir: './docs',
 });
